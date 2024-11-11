94bb24250de9acf6d6ff5419b73f4bb9f1350f33
deDg0d <76174644+deDg0d@users.noreply.github.com>
2024-11-11 12:03:29 -0800

    packages added

---
+++ packages_used/packages.py
@@ -0,0 +1,14 @@
+#danial ramezani
+###numpy version required = 1.23.0
+
+import pandas as pd
+import numpy as np
+from numba import jit,cuda,njit
+import time
+import math
+import random
+import matplotlib.pyplot as plt
+from collections import Counter
+import warnings
+from collections import OrderedDict
+import copy
